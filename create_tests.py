6a727f61c81821fa8b8babcf3b7ce072a226a3b8
smallmacy <55838866+smallmacy@users.noreply.github.com>
2022-09-02 16:22:03 +0300

    Add files via upload

--- create_tests.py
+++ create_tests.py
@@ -1,36 +1,90 @@
-import random
+import math
 from tests import *
 
-NUM_OF_TESTS = 1000
-STRAND_LENGTH = 10000
-WINDOW_SIZE = 50
 REAL_LENGTH = 20
-MAX_READ_AMOUNT = 10000
+NUM_OF_TESTS = 1000
 
 
-def generate_strand(letters):
+def test_different_strand_lengths(letters):
+    read_size = 1000
+    length = 100000
+    test_type = "strand_length"
+    count = 1
+    test_num = 100
+    print("started test_different_strand_lengths!")
+
+    while length <= 1000000:
+        print(f"test number {count}")
+        directory = f"input{count}"
+        strand = generate_strand(letters, length)
+        do_test(strand, length, read_size, test_type, directory, test_num)
+        length += 100000
+        count += 1
+
+
+def test_different_read_sizes(letters):
+    strand_length = 10000
+    read_size = 200
+    test_type = "read_size"
+    count = 4
+    test_num = 1000
+    strand = generate_strand(letters, strand_length)
+    print("started test_different_read_sizes!")
+
+    while read_size <= 500:
+        print(f"test number {count}")
+        directory = f"input{count}"
+        do_test(strand, strand_length, read_size, test_type, directory, test_num)
+        read_size += 50
+        count += 1
+
+
+def do_test(strand, strand_length, read_size, test_type, directory, test_num):
+    for k in range(math.ceil(strand_length / read_size), strand_length - read_size + 1, read_size):
+        success_amount = 0
+        no_cover_counter = 0
+
+        for _ in range(0, test_num):
+            reads, unused_letters = generate_k_reads(strand, k, strand_length, read_size)
+            if unused_letters > 0:
+                no_cover_counter += 1
+                continue
+            success_amount += final_algorithm(strand_length, read_size, REAL_LENGTH, k, strand, reads)
+
+        input_file = open(f"{test_type}/{directory}/test{k}.txt", "w")
+        input_file.write(f"{k}\n")
+        input_file.write(f"{no_cover_counter}\n")
+        input_file.write(f"{success_amount / NUM_OF_TESTS}\n")
+        if NUM_OF_TESTS - no_cover_counter != 0:
+            input_file.write(str(success_amount / (NUM_OF_TESTS - no_cover_counter)))
+        else:
+            input_file.write(str(0.0))
+        input_file.close()
+
+
+def generate_strand(letters, strand_length):
     strand = ""
-    for _ in range(0, STRAND_LENGTH):
+    for _ in range(0, strand_length):
         new_letter = random.choice(letters)
         strand = "".join([strand, new_letter])
     return strand
-  
 
-def generate_k_reads(strand, k):
-    unused_letters = STRAND_LENGTH
-    unused_arr = [False] * STRAND_LENGTH
-    reads_starts = [False] * (STRAND_LENGTH - WINDOW_SIZE + 1)
+
+def generate_k_reads(strand, k, strand_length, read_size):
+    unused_letters = strand_length
+    unused_arr = [False] * strand_length
+    reads_starts = [False] * (strand_length - read_size + 1)
     reads = []
 
     while k > 0:
-        window_start = random.randint(0, STRAND_LENGTH - WINDOW_SIZE)
+        window_start = random.randint(0, strand_length - read_size)
         if reads_starts[window_start]:
             continue
         reads_starts[window_start] = True
         k -= 1
-        reads.append(strand[window_start:window_start + WINDOW_SIZE])
+        reads.append(strand[window_start:window_start + read_size])
 
-        for j in range(window_start, window_start + WINDOW_SIZE):
+        for j in range(window_start, window_start + read_size):
             if not unused_arr[j]:
                 unused_letters -= 1
                 unused_arr[j] = True
@@ -40,29 +94,9 @@ def generate_k_reads(strand, k):
 
 def main():
     letters = ['A', 'C', 'G', 'T']
-    strand = generate_strand(letters)
-    print(f"strand: {strand}")
 
-    for k in range(200, MAX_READ_AMOUNT, 100):
-        success_amount = 0
-        no_cover_counter = 0
-
-        for _ in range(0, NUM_OF_TESTS):
-            reads, unused_letters = generate_k_reads(strand, k)
-            if unused_letters > 0:
-                no_cover_counter += 1
-                continue
-            success_amount += final_algorithm(STRAND_LENGTH, WINDOW_SIZE, REAL_LENGTH, k, strand, reads)
-
-        input_file = open(f"inputs/input{k}.txt", "w")
-        input_file.write(f"{k}\n")
-        input_file.write(f"{no_cover_counter}\n")
-        input_file.write(f"{success_amount / NUM_OF_TESTS}\n")
-        if NUM_OF_TESTS - no_cover_counter != 0:
-            input_file.write(str(success_amount / (NUM_OF_TESTS - no_cover_counter)))
-        else:
-            input_file.write(str(0.0))
-        input_file.close()
+    test_different_read_sizes(letters)
+    test_different_strand_lengths(letters)
 
 
 if __name__ == "__main__":
--- tests.py
+++ tests.py
@@ -5,10 +5,20 @@ from Utilities import *
 from FinalDirectedGraph import *
 from DynamicPrograming import *
 
-NUM_OF_TESTS = 300
+NUM_OF_TESTS = 100
+NUM_OF_SUCCESSES = 0
+NUM_OF_VERTICES = {}
+TESTS_FAILED = []
+TIME_I = 0
+TIME_G = 0
+TIME_DYNAMIC = 0
+CURR_TEST = 0
+same = 0
 
 
 def final_algorithm(strand_len, read_size, real_edge_length, number_of_reads, original_strand="", reads_lst=None):
+    global TIME_I, TIME_G, TESTS_FAILED, NUM_OF_SUCCESSES, NUM_OF_VERTICES, TIME_DYNAMIC, same
+    success = False
     # initialize data
     start_time = time.time()
 
@@ -21,7 +31,7 @@ def final_algorithm(strand_len, read_size, real_edge_length, number_of_reads, or
     if reads_lst is None:
         reads_lst_to_use = read_random_input_improve2(original_strand_to_use, strand_len, read_size, number_of_reads)
 
-    print_start_msg(original_strand_to_use)
+    # print_start_msg(original_strand_to_use)
 
     # time for initialization data
     # get_current_time(start_time)
@@ -29,6 +39,9 @@ def final_algorithm(strand_len, read_size, real_edge_length, number_of_reads, or
     # build induced graph
     data_for_induced_graph = AllOverlapGraphOptimizedBYAlex(read_size, real_edge_length, reads_lst_to_use)
     induced_graph = data_for_induced_graph.build_induced_graph_from_data()
+    f_i = time.time()
+    finished_i = time.time() - start_time
+    TIME_I += finished_i
     # induced_graph.print_graph("Induced Graph")
 
     # print("after induced graph:")
@@ -38,29 +51,44 @@ def final_algorithm(strand_len, read_size, real_edge_length, number_of_reads, or
 
     # build final directed graph
     final_directed_graph = FinalDirectedGraph(induced_graph, original_strand_to_use)
+    finished_g = time.time() - f_i
+    f_g = time.time()
+    TIME_G += finished_g
     # print("after final directed graph:")
-    final_directed_graph.print_graph("Final Directed Graph")
-    get_current_time(start_time)
+    # final_directed_graph.print_graph("Final Directed Graph")
+    # get_current_time(start_time)
     string_list_candidate = create_guesses(final_directed_graph, strand_len)
+    TIME_DYNAMIC += time.time() - f_g
 
     if not string_list_candidate or original_strand not in string_list_candidate:
         final_directed_graph.is_success = False
+        TESTS_FAILED.append(CURR_TEST)
     else:
         final_directed_graph.is_success = True
-    print_success_or_failure(final_directed_graph.is_success)
-    get_current_time(start_time)
+        success = True
+        NUM_OF_SUCCESSES += 1
+    # print_success_or_failure(final_directed_graph.is_success)
+    # get_current_time(start_time)
 
+    if final_directed_graph.num_of_vertices in NUM_OF_VERTICES:
+        NUM_OF_VERTICES[int(final_directed_graph.num_of_vertices)] += 1
+    else:
+        NUM_OF_VERTICES[int(final_directed_graph.num_of_vertices)] = 1
     # time of building the final directed_graph (should be very fast)
 
     # create_guesses(final_directed_graph, original_strand_to_use)
+    return success
 
 
 if __name__ == '__main__':
     time_sum = 0
-    for i in range(0, 300):
+    for i in range(0, NUM_OF_TESTS):
+        if i % 10 == 0:
+            print(f"Test Number {i}")
+        CURR_TEST = i
         f = open(f"input{i}.txt", "r")
         str_len = 10000
-        window_size = 100
+        window_size = 50
         real_edge_len = 20
         original_str = f.readline().strip()
         reads_list = []
@@ -72,4 +100,22 @@ if __name__ == '__main__':
         final_algorithm(str_len, window_size, real_edge_len, num_of_reads, original_str, reads_list)
         finish_alg = time.time()
         time_sum += finish_alg - start_alg
-    print(f"total algorithm time: {time_sum}")
+    stats_file = open("stats2.txt", "w")
+    stats_file.write("Paths num\t\t Occurences\n")
+    for key, val in NUM_OF_VERTICES.items():
+        stats_file.write(f"{key}\t\t\t {val}\n")
+
+    stats_file.write("\n\n")
+    stats_file.write(f"Overall success rate: \t\t\t\t {NUM_OF_SUCCESSES / (NUM_OF_SUCCESSES + len(TESTS_FAILED))}\n")
+    if len(TESTS_FAILED) == 0:
+        stats_file.write("No tests failed!\n")
+    else:
+        stats_file.write("Tests failed: \n")
+        for fail in TESTS_FAILED:
+            stats_file.write(f"{fail}\n")
+
+    stats_file.write("\n\n")
+    stats_file.write(f"Overall time to create graph I: {TIME_I}\n")
+    stats_file.write(f"Overall time to create graph G**: {TIME_G}\n")
+    stats_file.write(f"Overall time to do dynamic programming: {TIME_DYNAMIC}\n")
+    stats_file.write(f"total algorithm time: {time_sum}\n")
